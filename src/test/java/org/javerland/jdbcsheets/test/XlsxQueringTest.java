334b351e8006e1bf3aa8179ac929edc5f5577862
juraj.pacolt <juraj.pacolt@assecosol.com>
2025-01-12 22:54:40 +0100

    documentation has been added

--- src/test/java/org/javerland/jdbcsheets/test/XlsxQueringTest.java
+++ src/test/java/org/javerland/jdbcsheets/test/XlsxQueringTest.java
@@ -17,7 +17,7 @@ public class XlsxQueringTest {
         final String expected = "Melissa,Perry,Valerie,Grant,Steven,Stone,Jessica,Rosales,Tyler,Sandoval,";
         try (Connection conn = DriverManager.getConnection(TestConstants.URL_WITH_FILE)) {
             try (Statement stmt = conn.createStatement()) {
-                try (ResultSet rs = stmt.executeQuery("select A as One1,B,C,D,E from Sheet1 limit 5 offset 0")) {
+                try (ResultSet rs = stmt.executeQuery("select A as Col1,B,C,D,E from Sheet1 limit 5 offset 0")) {
                     StringBuilder sb = new StringBuilder();
                     while (rs.next()) {
                         String name = rs.getString("One1");
